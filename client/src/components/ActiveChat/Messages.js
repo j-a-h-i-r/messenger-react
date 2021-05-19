9b7427d327f840fd8aa865e5afbb0dad2c3cbc96
Jahirul Islam <jahir.ewubd@gmail.com>
2021-05-20 02:32:19 +0600

    Stop long conversations from going out of screen

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  messageContainer: {
+    height: "75vh",
+    overflowY: "auto",
+  },
+}));
 
 const Messages = (props) => {
+  const classes = useStyles();
   const { messages, otherUser, userId } = props;
 
   return (
-    <Box>
+    <Box className={classes.messageContainer}>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
